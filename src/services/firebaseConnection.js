9d8f648ef5bb1cf88dede78099754f65a3c88d53
Felipe Alves <felipeaamodio@gmail.com>
2022-01-31 04:49:04 -0300

    configurado o firebase

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBinL7ui-gMaa1Q2tUMZ8Zzrta9KL7IFfw",
+    authDomain: "tarefas-fa301.firebaseapp.com",
+    projectId: "tarefas-fa301",
+    storageBucket: "tarefas-fa301.appspot.com",
+    messagingSenderId: "1034199439490",
+    appId: "1:1034199439490:web:3c2cebbcb65c4163c7bc9b"
+  };
+
+  //se não tiver nenhuma conexão aberta, irei iniciar uma conexão
+  if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig)
+  }
+
+export default firebase;
